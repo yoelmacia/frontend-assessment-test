0af3c0100f3daa3eaabb481c06916362b28c15bf
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-11-27 13:19:04 +0100

    Fetch hotels data from the fake API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
-console.log('welcome on board!')
+const axios = require("axios");
+function fetchData() {
+  axios("http://fake-hotel-api.herokuapp.com/api/hotels")
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+fetchData();
