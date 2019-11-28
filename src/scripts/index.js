f8d5e7f0b0993128c038de40ec7c0131b7eb8f6f
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-11-28 14:43:42 +0100

    Add unordered list of hotels
    Create element for every field of the information
    got it from the API, append elements to the
    unordered list.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,54 @@
 const axios = require("axios");
 import "../styles/index.scss";
+
+function createNodeWithClass(element, className) {
+  let node = document.createElement(element);
+  node.className = className;
+  return node;
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById("hotels");
+const url =
+  "http://fake-hotel-api.herokuapp.com/api/hotels?count=10&no_error=true";
+
 function fetchData() {
-  axios("http://fake-hotel-api.herokuapp.com/api/hotels")
+  axios(url)
     .then(response => {
-      console.log(response);
+      let hotels = response.data;
+      return hotels.map(hotel => {
+        let li = createNodeWithClass("li", "grid-item"),
+          divName = createNodeWithClass("div", "divName"),
+          divCity = createNodeWithClass("div", "divCity"),
+          divCountry = createNodeWithClass("div", "divCountry"),
+          divDescription = createNodeWithClass("div", "divDescription"),
+          divPrice = createNodeWithClass("div", "divPrice"),
+          divRating = createNodeWithClass("div", "divRating"),
+          divStars = createNodeWithClass("div", "divStars"),
+          img = createNodeWithClass("img", "image");
+
+        divName.innerHTML = `Name: ${hotel.name}`;
+        divCity.innerHTML = `City: ${hotel.city}`;
+        divCountry.innerHTML = `Country : ${hotel.country}`;
+        divDescription.innerHTML = `Description: ${hotel.description}`;
+        divPrice.innerHTML = `Price: ${hotel.price}`;
+        divRating.innerHTML = `Rating: ${hotel.rating}`;
+        divStars.innerHTML = `Stars: ${hotel.stars}`;
+        img.src = `${hotel.images[0]}`;
+
+        append(li, img);
+        append(li, divName);
+        append(li, divCity);
+        append(li, divCountry);
+        append(li, divDescription);
+        append(li, divPrice);
+        append(li, divRating);
+        append(li, divStars);
+        append(ul, li);
+      });
     })
     .catch(error => {
       console.log(error);
